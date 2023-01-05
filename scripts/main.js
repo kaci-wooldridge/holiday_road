ccc2712a0df50f7b3e1593f0dfdef84734081615
Kaci Wooldridge <kaci.school.info@gmail.com>
2023-01-05 12:48:23 -0600

    linking api's

---
+++ scripts/main.js
@@ -0,0 +1,21 @@
+import {
+	fetchBizarres,
+	fetchEateries,
+	fetchParks,
+	fetchWeather,
+} from './data/DataAccess.js'
+
+const mainContainer = document.querySelector('#container')
+
+const render = () => {
+	fetchParks()
+		.then(() => fetchWeather())
+		.then(() => fetchBizarres())
+		.then(() => fetchEateries())
+}
+
+render()
+
+mainContainer.addEventListener('stateChanged', (customEvent) => {
+	render()
+})
--- scripts/parks/ParkProvider.js
+++
@@ -1,3 +0,0 @@
-import apiKeys from "../Settings.js";
-
-const npsAPIKey = apiKeys.npsKey;
