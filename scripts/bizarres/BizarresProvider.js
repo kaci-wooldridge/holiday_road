f3413d21a826ee871753685c9f0bc252f2a38092
Will Hedges <wshedges@outlook.com>
2023-01-06 15:22:44 -0600

    changing bizarres dropdown now shows the destination name in the itinerary

--- scripts/bizarres/BizarresProvider.js
+++ scripts/bizarres/BizarresProvider.js
@@ -2,6 +2,7 @@ import { getBizarres } from "../data/DataAccess.js";
 
 export const BizarresDropdown = () => {
     const bizarres = getBizarres();
+
     return `
         <div class="dropdown" id="bizarres__dropdown">
             <select>
@@ -15,3 +16,13 @@ export const BizarresDropdown = () => {
         </div>
     `;
 };
+
+const mainContainer = document.querySelector("#container");
+
+mainContainer.addEventListener("change", () => {
+    const selectedBizarre = document.querySelector(
+        "#bizarres__dropdown option:checked"
+    ).text;
+
+    document.querySelector(".chosenBizarre").innerHTML = selectedBizarre;
+});
