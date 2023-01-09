5ff2040d231c2571c01bc2804d896f526f78b2d3
Ben Hoberg <benjhoberg@gmail.com>
2023-01-09 09:41:32 -0600

    park itinerary is now showing in itinerary review and fetchWeather is now set up for specific lat and lon values

--- scripts/HolidayRoad.js
+++ scripts/HolidayRoad.js
@@ -1,44 +1,37 @@
 import { BizarresDropdown } from "./bizarres/BizarresProvider.js";
-import { ParkProvider } from "./parks/ParkProvider.js";
+import { ParkProvider, DisplayPark } from "./parks/ParkProvider.js";
 import { eateryDropdown } from './eateries/EateryProvider.js'
 
 
 export const HolidayRoad = () => {
-	return `
+  return `
       <h2>Holiday Road</h2>
     
       <div class="dropdownBoxes">
-        <div class="parkDropdown dropdown">
-          ${ParkProvider()}
-        </div>
-  
+        ${ParkProvider()}
         ${BizarresDropdown()}
         ${eateryDropdown()}
-      
       <div class="mainContent">
         <div class="chosenOptions">
           <div class="optionsDisplay">
             <h3>Your Itinerary</h3>
-            
-            <div class="chosenPark chosen">Park</div>
-            <div class="chosenBizarre chosen">Bizarre </div>
-            <div class="chosenEatery chosen">Eatery</div>
-        <div class="detailsDisplay"></div>
+            <div class="chosenPark">
+            ${DisplayPark()}
+            </div>
+            <div class="chosenBizarre">Bizarre </div>
+            <div class="chosenEatery">Eatery</div>
+            <div class="detailsDisplay"></div>
   
-          <button style="width:100px" class="saveButton">SAV3</button>
+            <button style="width:100px" class="saveButton">SAV3</button>
           </div>
   
           <div class="weatherDisplay">
-          <h3>Weather</h3>
+              <h3>Weather</h3>
           </div>
-        </div>
-  
-        <div class="savedOptions">
-          <h3>Saved Itinerary List</h3>
-          
-        </div>
-  
-  
+          </div>
+            <div class="savedOptions">
+              <h3>Saved Itinerary List</h3>
+            </div>
       </div>
       `
 }
--- scripts/parks/ParkProvider.js
+++ scripts/parks/ParkProvider.js
@@ -1,15 +1,75 @@
-import { getParks } from "../data/DataAccess.js";
+import { getParks, transientItineraryObj, fetchWeather } from "../data/DataAccess.js";
 
 export const ParkProvider = () => {
     const parks = getParks();
 
-    return `<select class="dropdown" id="parks__dropdown">
+    return `<div class="parkDropdown">
+            <select class="dropdown" id="parks__dropdown">
             <option value="0">Select a Park</option>
             ${parks.map(
-                (park) => {
-                    return `<option value="${park.id}">${park.fullName}</option>`
-            }
-        )
-    }
-</select>`
+        (park) => {
+            return `<option value="${park.parkCode}">${park.fullName}</option>`
+        }
+    )
+        }
+</select>
+</div>`
 }
+
+const pushParkObjToTransientItineraryObj = (parkKode) => {
+    transientItineraryObj.parkId = parkKode
+
+    const parks = getParks()
+
+    parks.map(
+        (park) => {
+            if (parkKode === park.parkCode) {
+                transientItineraryObj.lat = park.latitude,
+                transientItineraryObj.lon = park.longitude
+            }
+        }
+    )
+    
+    const lattitude = transientItineraryObj.lat
+    const longitude = transientItineraryObj.lon
+
+    fetchWeather(lattitude, longitude)
+}
+
+export const DisplayPark = () => {
+    const parks = getParks()
+    return parks.map(
+        (park) => {
+            if (park.parkCode === transientItineraryObj.parkId) {
+                return `${park.fullName}`
+            }
+        }
+    ).join("")
+
+
+}
+
+export const DisplayWeather = () => {
+
+}
+
+document.addEventListener(
+    "change",
+    (event) => {
+
+        const mainContainer = document.querySelector('#container')
+
+        const clicked = event.target
+
+        if (clicked.id === "parks__dropdown") {
+            const parkCode = clicked.value
+
+            pushParkObjToTransientItineraryObj(parkCode)
+
+            mainContainer.dispatchEvent(new CustomEvent("stateChanged"))
+        }
+
+    }
+)
+
+
