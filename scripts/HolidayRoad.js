f225fa4c9e73c5c12663b46a92e45cf3f5783718
Kaci Wooldridge <kaci.school.info@gmail.com>
2023-01-09 14:05:43 -0600

    fixed html

--- scripts/HolidayRoad.js
+++ scripts/HolidayRoad.js
@@ -18,12 +18,13 @@ export const HolidayRoad = () => {
         <div class="chosenOptions">
           <div class="optionsDisplay">
             <h3>Your Itinerary</h3>
-          
             
             <div class="chosenPark"></div>
             <div class="chosenBizarre"></div>
             <div class="chosenEatery"></div>
-        <div class="detailsDisplay"></div>
+          </div>
+
+          <div class="detailsDisplay"></div>
   
           <button style="width:100px" class="saveButton">SAV3</button>
           </div>
@@ -31,11 +32,11 @@ export const HolidayRoad = () => {
           <div class="weatherDisplay">
           <h3>Weather</h3>
           </div>
+
         </div>
   
         <div class="savedOptions">
           <h3>Saved Itinerary List</h3>
-          
         </div>
   
   
